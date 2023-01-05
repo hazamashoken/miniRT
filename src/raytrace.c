25fa7f8c11a397d7d18b889418f4309b506ccbf0
Andre Bossel <abossel@gmail.com>
2023-01-06 00:02:17 +0700

    shadows

--- src/raytrace.c
+++ src/raytrace.c
@@ -6,7 +6,7 @@
 /*   By: abossel <abossel@student.42bangkok.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 08:18:40 by abossel           #+#    #+#             */
-/*   Updated: 2023/01/05 20:30:06 by abossel          ###   ########.fr       */
+/*   Updated: 2023/01/06 00:00:44 by abossel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,49 @@ float	phong_lighting(t_ray *r, t_hit *h, t_mat *m, t_v3 light_dir)
 	return (intensity);
 }
 
+/*
+ * calculate diffuse lighting for a single light source
+ * I = Kd * (N dot L)
+ * N = surface normal
+ * L = unit vector from intersection point and light
+ * Kd = diffuse reflectivity
+ */
+float	diffuse_lighting(t_ray *r, t_hit *h, t_mat *m, t_v3 light_dir)
+{
+	float	intensity;
+
+	intensity = m->diffuse * v3dot(v3neg(light_dir), h->normal);
+	intensity = clamp(intensity, 0.0f, 1.0f);
+	return (intensity);
+}
+
+int	cast_ray(t_ray r);
+
+int	cast_mirror_ray(t_ray *r, t_hit *h)
+{
+	t_ray	reflect_ray;
+
+	reflect_ray.direction = v3norm(v3reflect(r->direction, h->normal));
+	reflect_ray.origin = h->point;
+	return (cast_ray(reflect_ray));
+}
+
+int	cast_shadow_ray(t_ray r)
+{
+	t_hit	h;
+
+	h = sphere_hit(r, v3new(0.0f, 3.5f, 1.0f), 0.5f);
+	if (h.distance >= 0.0f)
+		return (1);
+	h = sphere_hit(r, v3new(0.0f, 3.25f, -0.5f), 0.5f);
+	if (h.distance >= 0.0f)
+		return (1);
+	h = sphere_hit(r, v3new(0.0f, 10.0f, 0.0f), 2.0f);
+	if (h.distance >= 0.0f)
+		return (1);
+	return (0);
+}
+
 int	cast_ray(t_ray r)
 {
 	t_hit	h;
@@ -47,27 +90,42 @@ int	cast_ray(t_ray r)
 	t_v3	light;
 	t_v3	light_dir;
 	t_mat	m;
+	t_ray	sr;
 
 	m.specular = 1.5f;
-	m.diffuse = 0.6f;
+	m.diffuse = 0.8f;
 	m.ambient = 0.6f;
 	m.shine = 125.0f;
 	light = v3new(0.0f, 0.0f, 20.0f);
 	light_dir = v3norm(v3sub(h.point, light));
-	h = sphere_hit(r, v3new(1.5f, 3.0f, 0.0f), 0.5f);
+
+	h = sphere_hit(r, v3new(0.0f, 3.5f, 1.0f), 0.5f);
 	if (h.distance != -1.0f)
 	{
+		sr.origin = h.point;
+		sr.direction = v3neg(light_dir);
+		if (cast_shadow_ray(sr))
+			return (rgb(0, 0, 0));
 		return (rgb(0, 0, 255.0f * phong_lighting(&r, &h, &m, light_dir)));
 	}
-	h = sphere_hit(r, v3new(-1.0f, 5.0f, 1.0f), 0.5f);
+	h = sphere_hit(r, v3new(0.0f, 3.25f, -0.5f), 0.5f);
 	if (h.distance != -1.0f)
 	{
+		sr.origin = h.point;
+		sr.direction = v3neg(light_dir);
+		if (cast_shadow_ray(sr))
+			return (rgb(0, 0, 0));
 		return (rgb(0, 255.0f * phong_lighting(&r, &h, &m, light_dir), 0));
 	}
 	h = sphere_hit(r, v3new(0.0f, 10.0f, 0.0f), 2.0f);
 	if (h.distance != -1.0f)
 	{
-		return (rgb(255.0f * phong_lighting(&r, &h, &m, light_dir), 0, 0));
+		sr.origin = h.point;
+		sr.direction = v3neg(light_dir);
+		if (cast_shadow_ray(sr))
+			return (rgb(0, 0, 0));
+		//return (rgb(255.0f * diffuse_lighting(&r, &h, &m, light_dir), 0, 0));
+		return (cast_mirror_ray(&r, &h));
 	}
 	return (rgb(0, 0, 0));
 }
